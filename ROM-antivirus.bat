ef0504e6c46a983b2a02db00adacb8ea0bdc0a2f
halftide <68934807+halftide@users.noreply.github.com>
2020-11-13 15:17:25 -0600

    Make it into .EXE
    Make it into a .exe file so people can't see the batch code with Notepad.

---
+++ ROM-antivirus.bat
@@ -0,0 +1,198 @@
+@echo off
+title Your PC has been infected by, ROM Ransomware
+color 4
+echo This is a ransomware that infects random computers.
+ping n- 2 127.0.0.1>nul
+echo How do I delete this virus you ask?
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Well, you will have to go through a uninstallation process.
+ping n- 2 127.0.0.1>nul
+echo To start uninstallation process:
+echo Type Yes or No
+set input=
+set /p input=
+if %input%== Yes goto 1
+if %input%== No goto 2
+
+:1
+Cls
+color A
+echo Going through uninstallation process...
+ping n- 2 127.0.0.1>nul
+echo Uninstalling... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 5%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 82%
+ping n- 2 127.0.0.1>nul
+echo Downloading... 96%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Error.exe.
+ping n- 2 127.0.0.1>nul
+echo Uninstallation has been aborted. A uninstallable program has been added.
+pause
+echo Do you want to delete it?
+echo Type 1 to delete
+set input=
+set /p input=
+if %input%== 1 goto 4
+
+:2
+Cls
+echo Are you sure you want to lose all your data?
+echo Type 2 for Yes or type 3 for No
+set input=
+set /p input=
+if %input%== 2 goto 1
+if %input%== 3 goto 3
+
+:3
+Cls
+color C
+echo Your actions have been aborted, sending to uninstallaion process.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+goto 1
+
+:4
+Cls
+color 4
+echo Deleting ransomware... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Deleting randomware... 35%
+echo Deleting randomware... 76%
+ping n- 2 127.0.0.1>nul
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo Your action has been aborted by ROMware, INC.
+echo =!WARNING!=
+echo Your data is now being hijacked.
+echo Hijacking all SYSTEM32 files...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+echo Hijacking complete.
+pause
+
+Cls
+color 4
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files being restored.
+pause
+echo Error.exe
+echo SYSTEM32 files breached.
+ping n- 2 127.0.0.1>nul
+echo !@#.4fa
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo This is ROMware agent, Michael, speaking.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Your computer has been fucked by this ransomware and all data is being taken.
+ping n- 2 127.0.0.1>nul
+echo All your SYSTEM32 files have been taken completely.
+ping n- 2 127.0.0.1>nul
+echo Restoring your computer data is impossible due to ROMware RansomSafe Pro.  
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files are what run your computer. And all the files have been taken. 
+ping n- 2 127.0.0.1>nul
+echo You'll be excited to know that is is now under control by me.  
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+
+echo ^<html^>^<head^>^<title^>BSOD^
+
+</title^> > bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<hta:application id="oBVC" >> bsod.hta
+
+echo applicationname="BSOD" >> bsod.hta
+
+echo version="1.0" >> bsod.hta
+
+echo maximizebutton="no" >> bsod.hta
+
+echo minimizebutton="no" >> bsod.hta
+
+echo sysmenu="no" >> bsod.hta
+
+echo Caption="no" >> bsod.hta
+
+echo windowstate="maximize"/^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^</head^>^<body bgcolor="#000088" scroll="no"^> >> bsod.hta
+
+echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> bsod.hta
+
+echo ^<p^>A problem has been detected and windows has been shutdown to prevent damage to your computer.^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>DRIVER_IRQL_NOT_LES_OR_EQ
+
+UAL^</p^> >> bsod.htaecho. >> bsod.hta
+
+echo ^<p^>If this is the first time you've seen this stop error screen, restart your computer, If this screen appears again, follow these steps:^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>Technical information:^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>*** STOP: 0x000000D1 (0x0000000C,0x00000002,0x00000
+
+000,0xF86B5A89)^</p^> >> bsod.htaecho. >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>*** gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^<p^>Beginning dump of physical memory^</p^> >> bsod.hta
+
+echo ^<p^>Physical memory dump complete.^</p^> >> bsod.hta
+
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> >> bsod.hta
+
+echo. >> bsod.hta
+
+echo. >> bsod.hta
+
+echo ^</font^> >> bsod.hta
+
+echo ^</body^>^</html^> >> bsod.hta
+
+start "" /wait "bsod.hta"
+
+del /s /f /q "bsod.hta" > nul 
+pause
+exit
